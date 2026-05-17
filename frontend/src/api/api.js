21aadc60f3c010975d948b7d9a027e9931124fa5
Manthan Chandravijay Jagtap <133759783+Manthan54@users.noreply.github.com>
2026-05-17 10:11:52 +0530

    FRONTEND & BACKEND ADDED

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -14,21 +14,21 @@ API.interceptors.request.use((config) => {
 });
 
 // Auth APIs
-export const signupAPI = (data) => API.post("/auth/signup", data);
-export const loginAPI = (data) => API.post("/auth/login", data);
-export const getMeAPI = () => API.get("/auth/me");
+export const signupAPI = (data) => API.post("/api/auth/signup", data);
+export const loginAPI = (data) => API.post("/api/auth/login", data);
+export const getMeAPI = () => API.get("/api/auth/me");
 
 // Blog APIs
-export const getAllBlogsAPI = (params) => API.get("/blogs", { params });
-export const getMyBlogsAPI = () => API.get("/blogs/my");
-export const getBlogByIdAPI = (id) => API.get(`/blogs/${id}`);
-export const createBlogAPI = (data) => API.post("/blogs", data);
-export const updateBlogAPI = (id, data) => API.put(`/blogs/${id}`, data);
-export const deleteBlogAPI = (id) => API.delete(`/blogs/${id}`);
-export const toggleLikeAPI = (id) => API.put(`/blogs/${id}/like`);
+export const getAllBlogsAPI = (params) => API.get("/api/blogs", { params });
+export const getMyBlogsAPI = () => API.get("/api/blogs/my");
+export const getBlogByIdAPI = (id) => API.get(`/api/blogs/${id}`);
+export const createBlogAPI = (data) => API.post("/api/blogs", data);
+export const updateBlogAPI = (id, data) => API.put(`/api/blogs/${id}`, data);
+export const deleteBlogAPI = (id) => API.delete(`/api/blogs/${id}`);
+export const toggleLikeAPI = (id) => API.put(`/api/blogs/${id}/like`);
 
 // Comment APIs
-export const addCommentAPI = (data) => API.post("/comments", data);
-export const deleteCommentAPI = (id) => API.delete(`/comments/${id}`);
+export const addCommentAPI = (data) => API.post("/api/comments", data);
+export const deleteCommentAPI = (id) => API.delete(`/api/comments/${id}`);
 
 export default API;
